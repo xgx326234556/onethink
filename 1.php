f0aaed3702988c840d74f0b07f62fe02b52b9718
xx <qq@com>
2017-08-13 19:33:17 +0800

    1

--- 1.php
+++ 1.php
@@ -4,4 +4,10 @@ if($_GET['echostr']){
 }else{
     $xml=file_get_contents("php://input");
     file_put_contents('wchat.log',$xml);
+    $simpleXml=simplexml_load_string($xml);
+    $request = [];
+    foreach ($simpleXml as $name=>$value){
+        $request[$name] = (string)$value;
+    }
+   require 'message_text.xml';
 }
